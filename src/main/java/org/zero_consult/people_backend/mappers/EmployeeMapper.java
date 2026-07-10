3b73bdfeeea11a3050c4c61ed2e1eddd2a7cefb2
Mathias Ver Elst <mathias.ver.elst@gmail.com>
2026-07-10 20:41:24 +0200

    make optional properties of type Optional in IDL

--- src/main/java/org/zero_consult/people_backend/mappers/EmployeeMapper.java
+++ src/main/java/org/zero_consult/people_backend/mappers/EmployeeMapper.java
@@ -5,6 +5,7 @@ import org.zero_consult.people_backend.utils.DateUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class EmployeeMapper {
 
@@ -15,13 +16,13 @@ public class EmployeeMapper {
     private static Employee toIdl(org.zero_consult.people_backend.entities.Employee employee, List<String> ids) {
         ids.add(employee.getId());
         Employee mappedEmployee = new Employee();
-        mappedEmployee.setId(employee.getId());
-        mappedEmployee.setDepartment(DepartmentMapper.toIdl(employee.getDepartment()));
+        mappedEmployee.setId(employee.getId() != null ? Optional.of(employee.getId()) : Optional.empty());
+        mappedEmployee.setDepartment(employee.getDepartment() != null ? Optional.of(DepartmentMapper.toIdl(employee.getDepartment())) : Optional.empty());
         mappedEmployee.setEmail(employee.getEmail());
         mappedEmployee.setFirstName(employee.getFirstName());
-        mappedEmployee.setFunctionTitle(employee.getFunctionTitle());
+        mappedEmployee.setFunctionTitle(employee.getFunctionTitle() != null && !employee.getFunctionTitle().isEmpty() ? Optional.of(employee.getFunctionTitle()) : Optional.empty());
         mappedEmployee.setLastName(employee.getLastName());
-        mappedEmployee.setManager(employee.getManager() != null && !ids.contains(employee.getManager().getId()) ? EmployeeMapper.toIdl(employee.getManager(), ids) : null);
+        mappedEmployee.setManager(employee.getManager() != null && !ids.contains(employee.getManager().getId()) ? Optional.of(EmployeeMapper.toIdl(employee.getManager(), ids)) : null);
         mappedEmployee.setPhone(employee.getPhone());
         mappedEmployee.setStartDate(DateUtils.toIdl(employee.getStartDate()));
         mappedEmployee.setStatus(EmployeeStatusMapper.toIdl(employee.getStatus()));
@@ -30,12 +31,13 @@ public class EmployeeMapper {
 
     public static org.zero_consult.people_backend.entities.Employee toEntity(Employee employee) {
         org.zero_consult.people_backend.entities.Employee mappedEmployee = new org.zero_consult.people_backend.entities.Employee();
-        mappedEmployee.setDepartment(employee.getDepartment() != null ? DepartmentMapper.toEntity(employee.getDepartment()) : null);
+        employee.getDepartment().ifPresent((department) -> mappedEmployee.setDepartment(DepartmentMapper.toEntity(department)));
         mappedEmployee.setEmail(employee.getEmail());
         mappedEmployee.setFirstName(employee.getFirstName());
-        mappedEmployee.setFunctionTitle(employee.getFunctionTitle());
+        employee.getFunctionTitle().ifPresent(mappedEmployee::setFunctionTitle);
+        employee.getId().ifPresent(mappedEmployee::setId);
         mappedEmployee.setLastName(employee.getLastName());
-        mappedEmployee.setManager(employee.getManager() != null ? EmployeeMapper.toEntity(employee.getManager()) : null);
+        employee.getManager().ifPresent((manager) -> mappedEmployee.setManager(EmployeeMapper.toEntity(manager)));
         mappedEmployee.setPhone(employee.getPhone());
         mappedEmployee.setStartDate(DateUtils.fromIdl(employee.getStartDate()));
         mappedEmployee.setStatus(employee.getStatus() != null ? EmployeeStatusMapper.toEntity(employee.getStatus()) : null);
